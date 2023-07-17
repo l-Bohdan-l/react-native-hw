4144fdc7deac71fa71cb7cd9edbac006bd06c16c
Bohdan <bpapushoj@gmail.com>
2023-07-17 15:51:58 +0300

    Update RegistrationScreen.js

--- src/components/Screens/RegistrationScreen.js
+++ src/components/Screens/RegistrationScreen.js
@@ -5,22 +5,31 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import { useState } from "react";
 
 export const RegistrationScreen = () => { 
-    console.log('121212')
-    const [login, setLogin] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const initialState = {
+        login: '',
+        email: '',
+        password: '',
+    };
+    // const [login, setLogin] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
     const [hidePass, setHidePass] = useState(true);
     const [isShownKeyboard, setIsShownKeyboard] = useState(false);
+    const [state, setState] = useState(initialState);
 
-    const loginHandler = (text) => { 
-        setLogin(text.trim());
+    const loginHandler = (value) => { 
+        // setLogin(text.trim());
+       setState((prevState)=>({...prevState, login: value.trim()}))
     };
-    const emailHandler = (text) => { 
-        setEmail(text.trim());
+    const emailHandler = (value) => { 
+        // setEmail(text.trim());
+        setState((prevState)=>({...prevState, email: value.trim()}))
     };
-    const passwordHandler = (text) => {
-        setPassword(text.trim());
+    const passwordHandler = (value) => {
+        // setPassword(text.trim());
+        setState((prevState)=>({...prevState, password: value.trim()}))
     };
+
     const showPassword = (e) => {
         setHidePass(!hidePass);
     };
@@ -29,10 +38,12 @@ export const RegistrationScreen = () => {
         // if (!login || !email || !password) {
         //     return Alert.alert('Помилка', 'Заповніть всі поля')
         //  }
-        console.log('submit', login, email, password)
+        // console.log('submit', login, email, password)
+        console.log('submit state', state)
         console.log('show', isShownKeyboard)
         setIsShownKeyboard(false)
         Keyboard.dismiss()
+        setState(initialState)
     };
 
     const handleContainerTouch = (e) => { 
@@ -53,14 +64,28 @@ export const RegistrationScreen = () => {
                     }}></View>
                 <Text style={styles.title}>Реєстрація</Text>
                 <View style={styles.from}>
-                    <TextInput value={login} onChangeText={loginHandler} style={styles.input} placeholder="Логін" onFocus={() => {setIsShownKeyboard(true)}}/>
-                    <TextInput value={email} onChangeText={emailHandler} style={styles.input} keyboardType="email-address" placeholder="Адреса електронної пошти" onFocus={() => {setIsShownKeyboard(true)}}/>                
+                        <TextInput value={state.login}
+                            onChangeText={loginHandler}
+                            onEndEditing={() => { setIsShownKeyboard(false) }}
+                            style={styles.input}
+                            placeholder="Логін"
+                            onFocus={() => { setIsShownKeyboard(true) }} />
+                        <TextInput value={state.email}
+                            onChangeText={emailHandler}
+                            onEndEditing={() => { setIsShownKeyboard(false) }}
+                            style={styles.input}
+                            keyboardType="email-address"
+                            placeholder="Адреса електронної пошти"
+                            onFocus={() => { setIsShownKeyboard(true) }} />                
                     <View>
-                            <TextInput value={password} onChangeText={passwordHandler} style={{
+                            <TextInput value={state.password} onChangeText={passwordHandler} style={{
                                 ...styles.input,
                                 ...styles.lastInput,
                                 marginBottom: isShownKeyboard ? 10 : 43,
-                            }} secureTextEntry={hidePass} placeholder="Пароль" onFocus={() => { setIsShownKeyboard(true) }} /> 
+                            }}
+                                secureTextEntry={hidePass}
+                                onEndEditing={() => { setIsShownKeyboard(false) }}
+                                placeholder="Пароль" onFocus={() => { setIsShownKeyboard(true) }} /> 
                         <TouchableOpacity style={styles.showPassBtn} onPress={showPassword}>
                             <Text style={styles.showPassBtnText}>Показати</Text>    
                         </TouchableOpacity>
